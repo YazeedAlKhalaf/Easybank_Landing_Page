5e5654c861177e56248b97470bfd6103859684ad
Yazeed AlKhalaf <yazeedfady@gmail.com>
2020-07-22 01:11:08 +0300

    ➕ Add Animated Hamburger Menu & Overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+const btnHamburger = document.querySelector("#hamburger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+header.addEventListener("click", () => {
+  console.log("open hamburger");
+
+  if (header.classList.contains("open")) {
+    // close menu
+    header.classList.remove("open");
+    overlay.classList.remove("fade-in");
+    overlay.classList.add("fade-out");
+  } else {
+    // open menu
+    header.classList.add("open");
+    overlay.classList.remove("fade-out");
+    overlay.classList.add("fade-in");
+  }
+});
